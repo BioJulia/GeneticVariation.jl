d6fb3aa9cad3b2059b754b92b249e7f20ce4f395
Ben J. Ward <ward9250@gmail.com>
2017-06-16 11:01:01 +0100

    BCF IO
    * Added BCF code
    
    * Added tests
    
    * import GeneticVariation.VCF to BCF
    
    * Bio -> BioCore
    
    * BCF export
    
    * Bio.Var -> GeneticVariation

---
+++ src/bcf/bcf.jl
@@ -0,0 +1,20 @@
+# bcf.jl
+# ======
+#
+# A submodule for reading, writing, and working with BCF formatted files.
+#
+# This file is a part of BioJulia.
+# License is MIT: https://github.com/BioJulia/GeneticVariation.jl/blob/master/LICENSE
+
+module BCF
+
+import BioCore: BioCore, isfilled
+import GeneticVariation.VCF
+import BGZFStreams
+import BufferedStreams
+
+include("record.jl")
+include("reader.jl")
+include("writer.jl")
+
+end
