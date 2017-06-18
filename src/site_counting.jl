23153dc2f3c2da1f9dec3b8e01ae15f044f95279
Ben J. Ward <ward9250@gmail.com>
2017-06-18 19:21:26 +0100

    extended nibble mask

--- src/site_counting.jl
+++ src/site_counting.jl
@@ -42,6 +42,14 @@ immutable Transversion <: Mutation end
 # Bitparallel counting
 # --------------------
 
+@inline function nibble_mask(::Type{Certain}, x::UInt64)
+    return nibble_mask(enumerate_nibbles(x), 0x1111111111111111)
+end
+
+@inline function nibble_mask(::Type{Certain}, a::UInt64, b::UInt64)
+    return nibble_mask(Certain, a) & nibble_mask(Certain, b)
+end
+
 for A in (DNAAlphabet, RNAAlphabet)
     @eval begin
 
