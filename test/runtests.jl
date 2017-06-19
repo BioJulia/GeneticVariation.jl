3461dd4c1325b18d4e3b8e45e02038419ac83c32
Ben J. Ward <ward9250@gmail.com>
2017-06-19 17:35:41 +0100

    Added randomized tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,27 @@ function get_bio_fmt_specimens()
     end
 end
 
+function random_seq(n::Integer, nts, probs)
+    cumprobs = cumsum(probs)
+    x = Vector{Char}(n)
+    for i in 1:n
+        x[i] = nts[searchsorted(cumprobs, rand()).start]
+    end
+    return convert(String, x)
+end
+
+function random_seq{A<:Alphabet}(::Type{A}, n::Integer)
+    nts = alphabet(A)
+    probs = Vector{Float64}(length(nts))
+    fill!(probs, 1 / length(nts))
+    return BioSequence{A}(random_seq(n, nts, probs))
+end
+
+function random_interval(minstart, maxstop)
+    start = rand(minstart:maxstop)
+    return start:rand(start:maxstop)
+end
+
 include("vcf.jl")
 include("bcf.jl")
 include("site_counting.jl")
--- test/site_counting.jl
+++ test/site_counting.jl
@@ -38,4 +38,90 @@
             @test count(Mutated, rnaA, rnaB) == count(Mutated, rnaB, rnaA) == (6, 10)
         end
     end
+
+    @testset "Randomized tests" begin
+        # A test counting function which is naive.
+        @inline function testcount{P<:BioSequences.Position}(::Type{P}, a::BioSequence, b::BioSequence)
+            k = 0
+            @inbounds for idx in 1:min(endof(a), endof(b))
+                k += issite(P, a, b, idx)
+            end
+            return k
+        end
+
+        @inline function issite(::Type{Conserved}, a::BioSequence, b::BioSequence, idx)
+            return !(isambiguous(a[idx]) || isambiguous(b[idx])) && (a[idx] == b[idx])
+        end
+        @inline function issite(::Type{Mutated}, a::BioSequence, b::BioSequence, idx)
+            return !(isambiguous(a[idx]) || isambiguous(b[idx])) && (a[idx] != b[idx])
+        end
+
+        function testcounting{S<:Site}(::Type{S}, a, b)
+            @test count(S, a, b) == count(S, b, a) == testcount(S, a, b)
+        end
+
+        function testforencs(a::Int, b::Int, subset::Bool)
+            for alphabet in (DNAAlphabet, RNAAlphabet)
+                for _ in  1:50
+                    #println("TESTING SEQUENCES:")
+                    seqA = random_seq(alphabet{a}, rand(10:100))
+                    seqB = random_seq(alphabet{b}, rand(10:100))
+                    #=
+                    println("A seq: ", seqA)
+                    println("B seq: ", seqB)
+                    println("A length: ", length(seqA))
+                    println("B length: ", length(seqB))
+                    =#
+                    sa = seqA
+                    sb = seqB
+                    if subset
+                        intA = random_interval(1, length(seqA))
+                        intB = random_interval(1, length(seqB))
+                        subA = seqA[intA]
+                        subB = seqB[intB]
+                        #=
+                        println("A subset: ", intA)
+                        println("B subset: ", intB)
+                        println("A subseq: ", subA)
+                        println("B subseq: ", subB)
+                        =#
+                        sa = subA
+                        sb = subB
+                    end
+                    testcounting(Conserved, sa, sb)
+                    testcounting(Mutated, sa, sb)
+                end
+            end
+        end
+
+        @testset "Testing 4-bit seq pairs" begin
+            @testset "Full random sequences" begin
+                testforencs(4, 4, false)
+            end
+            @testset "Subset random sequences" begin
+                testforencs(4, 4, true)
+            end
+        end
+
+        @testset "Testing 2-bit seq pairs" begin
+            @testset "Full random sequences" begin
+                testforencs(2, 2, false)
+            end
+            @testset "Subset random sequences" begin
+                testforencs(2, 2, true)
+            end
+        end
+
+        @testset "Testing mixed bit seq pairs" begin
+            @testset "Full random sequences" begin
+                testforencs(4, 2, false)
+                testforencs(2, 4, false)
+            end
+            @testset "Subset random sequences" begin
+                testforencs(4, 2, true)
+                testforencs(2, 4, true)
+            end
+        end
+
+    end
 end
