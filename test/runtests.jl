e82a738483590bab1c16d3d62775c4e54b8630a7
Ben J. Ward <ward9250@gmail.com>
2017-06-26 17:05:26 +0100

    Import from BioCore.Testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,31 +2,20 @@ module TestGeneticVariation
 
 using Base.Test
 
-import BioCore.Testing.get_bio_fmt_specimens
+import BioCore.Testing:
+    get_bio_fmt_specimens,
+    random_seq,
+    random_interval
 using BioSequences, GeneticVariation
 import BufferedStreams: BufferedInputStream
 import IntervalTrees: IntervalValue
 import YAML
-
-function random_seq(n::Integer, nts, probs)
-    cumprobs = cumsum(probs)
-    x = Vector{Char}(n)
-    for i in 1:n
-        x[i] = nts[searchsorted(cumprobs, rand()).start]
-    end
-    return convert(String, x)
-end
 
 function random_seq{A<:Alphabet}(::Type{A}, n::Integer)
     nts = alphabet(A)
     probs = Vector{Float64}(length(nts))
     fill!(probs, 1 / length(nts))
     return BioSequence{A}(random_seq(n, nts, probs))
-end
-
-function random_interval(minstart, maxstop)
-    start = rand(minstart:maxstop)
-    return start:rand(start:maxstop)
 end
 
 fmtdir = get_bio_fmt_specimens()
