c9037613a7da457b0326d9d225e77358df6d7fd8
Ben J. Ward <ward9250@gmail.com>
2017-06-26 15:12:27 +0100

    Added qualification for ncbi_trans_table

--- src/ng86.jl
+++ src/ng86.jl
@@ -7,7 +7,7 @@
 # License is MIT: https://github.com/BioJulia/GeneticVariation.jl/blob/master/LICENSE.md
 
 const CDN = Union{BioSequences.DNACodon, BioSequences.RNACodon}
-const DEFAULT_TRANS = ncbi_trans_table[1]
+const DEFAULT_TRANS = BioSequences.ncbi_trans_table[1]
 immutable NG86 end
 
 """
