19599ad600de75cc1184b4770c2e7815b93005eb
Ben J. Ward <ward9250@gmail.com>
2017-06-26 16:13:46 +0100

    fixes for tests

--- test/ng86.jl
+++ test/ng86.jl
@@ -11,8 +11,8 @@
         s_ans = [0.0, 0.333, 1.0, 1.0, 0.333, 0.666]
         for i in 1:endof(codons)
             cdn = codons[i]
-            @test  == @test_approx_eq_eps GeneticVariation.expected(cdn)[1] s_ans[i] 1e-3
-            @test  == @test_approx_eq_eps GeneticVariation.expected(cdn)[2] n_ans[i] 1e-3
+            @test_approx_eq_eps GeneticVariation.expected(cdn)[1] s_ans[i] 1e-3
+            @test_approx_eq_eps GeneticVariation.expected(cdn)[2] n_ans[i] 1e-3
         end
     end
 
