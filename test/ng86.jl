2319f8845207298d83038cf0509a8127cb975842
Ben J. Ward <ward9250@gmail.com>
2017-06-27 12:02:17 +0100

    fix

--- test/ng86.jl
+++ test/ng86.jl
@@ -6,7 +6,7 @@
               kmer"TTT"]
 
     @testset "expected" begin
-        n_ans = [3.0, 3.666, 2.0, 2.0, 2.666]
+        n_ans = [3.0, 2.666, 2.0, 2.0, 2.666]
         s_ans = [0.0, 0.333, 1.0, 1.0, 0.333]
         for i in 1:endof(codons)
             cdn = codons[i]
