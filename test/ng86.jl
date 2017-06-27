a689c9f3a82da10ad16197590c87f2ab583d3ce2
Ben J. Ward <ward9250@gmail.com>
2017-06-27 12:24:20 +0100

    fix

--- test/ng86.jl
+++ test/ng86.jl
@@ -22,7 +22,7 @@
         end
         testobserved(kmer"CCC", kmer"CGC", (0.0, 1.0))
         testobserved(kmer"GGG", kmer"GGC", (1.0, 0.0))
-        testobserved(kmer"TTT", kmer"TAC", (0.5, 1.5))
+        testobserved(kmer"TTT", kmer"TAC", (1.0, 1.0))
         testobserved(kmer"TTT", kmer"GAC", (0.333, 2.666))
     end
 end
