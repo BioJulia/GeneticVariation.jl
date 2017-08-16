b340da180cac63343d7e3b70f53f349a9a7236df
Ben J. Ward <ward9250@gmail.com>
2017-08-16 11:25:37 +0100

    qualified count_pairwise name

--- src/nuc_div.jl
+++ src/nuc_div.jl
@@ -73,6 +73,6 @@ function nuc_div(sequences)
     frequencies = allele_frequencies(sequences)
     unique_sequences = collect(keys(frequencies))
     n = length(unique_sequences)
-    mutations = pdist(count_pairwise(Mutated, unique_sequences...))
+    mutations = pdist(BioSequences.count_pairwise(Mutated, unique_sequences...))
     return nuc_div(mutations, collect(values(frequencies)))
 end
