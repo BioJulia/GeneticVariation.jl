870f94084a712443d0ca277f1b52dd25dba68b55
Ben J. Ward <ward9250@gmail.com>
2017-08-16 18:09:31 +0100

    fix a dumb typo

--- src/nuc_div.jl
+++ src/nuc_div.jl
@@ -73,7 +73,7 @@ function nuc_div(sequences)
     frequencies = allele_frequencies(sequences)
     unique_sequences = collect(keys(frequencies))
     n = length(unique_sequences)
-    if n == 0
+    if n < 2
         return 0.0
     else
         mutations = pdist(BioSequences.count_pairwise(Mutated, unique_sequences...))
