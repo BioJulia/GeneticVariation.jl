7ae92d2e53cb2c72d9d247fe63dc80b6605b12a1
autodocs <autodocs>
2017-09-02 14:08:04 +0000

    build based on 2c2d9fa

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "GeneticVariation.jl",
     "category": "section",
-    "text": "(Image: Latest Release) (Image: GeneticVariation) (Image: GeneticVariation) (Image: License) (Image: BioJulia maintainer: bicycle1885) (Image: BioJulia maintainer: Ward9250)Development builds: (Image: Build Status) (Image: Build status) (Image: codecov)"
+    "text": "(Image: Latest Release) (Image: GeneticVariation) (Image: License) (Image: BioJulia maintainer: bicycle1885) (Image: BioJulia maintainer: Ward9250)Development builds: (Image: Build Status) (Image: Build status) (Image: codecov)"
 },
 
 {
