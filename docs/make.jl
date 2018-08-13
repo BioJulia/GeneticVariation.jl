255572700b3dcf1087c6afc79fb88769dba84183
Ben J. Ward <ward9250@gmail.com>
2018-08-13 12:22:57 +0100

    Remove NEWS file

--- docs/make.jl
+++ docs/make.jl
@@ -19,7 +19,7 @@ makedocs(
 
 deploydocs(
     repo = "github.com/BioJulia/GeneticVariation.jl.git",
-    julia = "0.6",
+    julia = "1.0",
     osname = "linux",
     target = "build",
     deps = nothing,
